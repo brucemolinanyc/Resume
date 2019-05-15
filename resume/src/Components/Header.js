46902dd0388c4c32b39393e27987c946ac79cda8
bmolina-nyc <brucemolinanyc@gmail.com>
2019-05-15 18:29:37 -0400

    updated footer, header, images

--- resume/src/Components/Header.js
+++ resume/src/Components/Header.js
@@ -35,7 +35,7 @@ class Header extends Component {
       <div className="row banner">
          <div className="banner-text">
             <h1 className="responsive-headline">I'm {name}.</h1>
-            <strong><h3>I'm a {city} based <span>{occupation}</span>. {description}.</h3></strong>
+            <strong><h3>{city} based <span>{occupation}</span>. {description}.</h3></strong>
             <hr />
             <ul className="social">
                {networks}
